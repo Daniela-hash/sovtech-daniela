9f3d4fe5e3b4cef78b24db4c385938dafd594836
Daniela Batev <dbatev.ot@gmail.com>
2022-12-01 21:39:47 +0200

    Add to Why

--- pages/why.js
+++ pages/why.js
@@ -18,8 +18,20 @@ export default function Home() {
         <h2>Let's dig into the WHY a little more...</h2>
         <br/>
         <br/>
-        <p>
-
+        <h2><em>Teamwork makes the Dreamwork</em></h2>
+        <p className={styles.card}>
+          The ability to join an established company that offers a Graduate Programme, 
+          promotes on-the-job learning and values mentorship aligns with my trajectory towards growth.
+        <br/>
+        <br/>
+          Similarities between my reasons for moving into Software Development and SovTech: 
+          <ul>
+            <li>The ability to solve people's problems and maximize their productivity.</li>
+            <li>I have an innate need to help people.</li>
+            <li>Oppportunity to collaborate with team members.</li>
+            <li>Have a place to practice and gain exposure to designing and building a variety of applications.</li>
+            <li>Allow for mentorship and the ability to be in an environement of mutual respect.</li>
+            </ul>          
         </p>
       </main>
     </div>
